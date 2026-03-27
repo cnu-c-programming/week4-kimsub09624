21724897cc0dc9a60f3e2212aa2c016b9ff6da6c
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-27 11:45:31 +0900

    Update 3-1.c

---
+++ code/3-1/3-1.c
@@ -0,0 +1,15 @@
+#inclue <stdio.h>
+
+int main() {
+  int n = 10;
+  
+  int *p = &n;
+
+  printf("address of n: %[\n", p);
+
+  printf("*p = %d\n", *p);
+
+  *p = 99;
+  printf("n = %d\n", n);
+  return 0;
+}
